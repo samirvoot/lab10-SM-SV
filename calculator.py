2ce624e3ddf0eff38f429cad7c8d19c0904ad03c
samirvoot <samirvoot@gmail.com>
2025-11-12 15:27:25 -0500

    changes

--- calculator.py
+++ calculator.py
@@ -5,7 +5,28 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError
+    return a / b
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
 
